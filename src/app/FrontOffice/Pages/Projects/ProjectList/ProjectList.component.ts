f80956051520e797564e290b9ce69076ff2dbb1e
Pedro Moreira <pedromoreiramore213@outlook.pt>
2024-02-16 13:25:15 +0000

    Add Filtering throw the Projects

--- src/app/FrontOffice/Pages/Projects/ProjectList/ProjectList.component.ts
+++ src/app/FrontOffice/Pages/Projects/ProjectList/ProjectList.component.ts
@@ -1,21 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { ProjectService } from '../../../Services/Project.service';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Project } from '../../../Data/Project';
 
 @Component({
   selector: 'app-project-list',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './ProjectList.component.html',
   styleUrl: './ProjectList.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements OnInit {
 
-  projects = this.projectService.getProjects();
+  projects = this.projectService.getProjects()
+  search = new FormControl("");
 
-  constructor(private projectService: ProjectService) {}
+  constructor(private projectService: ProjectService) { }
+
+  public ngOnInit(): void {
+    this.search.valueChanges.subscribe((value) => {
+      var allValues = this.projectService.getProjects();
+      if (value != "") {
+        allValues().map((v) => v.Name?.includes(value!) ?? v.Name);
+      } else {
+        this.projects = allValues;
+      }
+    });
+  }
 
 }
