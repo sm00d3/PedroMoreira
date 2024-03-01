9a2f38a454995d25dad50cc480e9cf8c4e34a020
Pedro Moreira <pedrosilvmore213@gmail.com>
2024-03-01 22:59:00 +0000

    Ajusts on DefaultLayout and ProfilePage

--- src/app/FrontOffice/Pages/Profile/Profile.component.ts
+++ src/app/FrontOffice/Pages/Profile/Profile.component.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink
   ],
   templateUrl: './Profile.component.html',
   styleUrl: './Profile.component.scss',
